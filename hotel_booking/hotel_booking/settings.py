4331c7923e185a05dbea138416e3500753e019af
jmass-ggg <jamesgurung690@gmail.com>
2026-03-02 15:25:00 +0545

    docker add

--- hotel_booking/hotel_booking/settings.py
+++ hotel_booking/hotel_booking/settings.py
@@ -74,23 +74,31 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'hotel_booking.wsgi.application'
-
+import os
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "hotelBook",
+#         "USER": "postgres",
+#         "PASSWORD": "Kanye@12",
+#         "HOST": "localhost",
+#         "PORT": "5432",
+#     }
+# }
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "hotelBook",
-        "USER": "postgres",
-        "PASSWORD": "Kanye@12",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "NAME": os.getenv("DB_NAME", "hotelBook"),
+        "USER": os.getenv("DB_USER", "postgres"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST", "db"),  
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
-}
-
-# Password validation
+}# Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -125,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"  
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
